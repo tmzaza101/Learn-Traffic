ba114da8f17900a54b1edc9d847984ea4bdb157d
BlackChaOs <tumitza@gmail.com>
2016-04-02 15:06:59 +0700

    20160402 - finish

--- app/src/main/java/aligns/sapphaya/blackchaos/alerttraffic/MainActivity.java
+++ app/src/main/java/aligns/sapphaya/blackchaos/alerttraffic/MainActivity.java
@@ -1,13 +1,37 @@
 package aligns.sapphaya.blackchaos.alerttraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //statement
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //bind widget คือการผูกความสัมพันธ์ของตัวแปร กับ widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        //การเก็บเหตุการณ์ click about me
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sound effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
+                mediaPlayer.start();
+            } //onclick
+        });
+    }//main mathod
+
+
+}//main Class
