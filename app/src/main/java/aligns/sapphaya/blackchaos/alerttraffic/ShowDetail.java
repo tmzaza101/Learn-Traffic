e349a3f5146d1c1f6e4fc68f368b7a2e98b696cf
BlackChaOs <tumitza@gmail.com>
2016-04-03 15:01:43 +0700

    Finish

--- app/src/main/java/aligns/sapphaya/blackchaos/alerttraffic/ShowDetail.java
+++ app/src/main/java/aligns/sapphaya/blackchaos/alerttraffic/ShowDetail.java
@@ -27,5 +27,11 @@ public class ShowDetail extends AppCompatActivity {
         int intImage = getIntent().getIntExtra("Icon", R.drawable.traffic_01);
         trafficImageView.setImageResource(intImage);
 
+        int intIndex = getIntent().getIntExtra("Index",0);
+        String[] detailStrings = getResources().getStringArray(R.array.detail);
+
+        detailTextView.setText(detailStrings[intIndex]);
+
+
     }//main method
 }// main class
---
+++ app/src/main/java/aligns/sapphaya/blackchaos/alerttraffic/SplashScreen.java
@@ -0,0 +1,28 @@
+package aligns.sapphaya.blackchaos.alerttraffic;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 6000);
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_start_horse);
+        mediaPlayer.start();
+    }//main method
+
+}//main class
