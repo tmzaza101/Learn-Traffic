84423ea35ea196d9fdefaf96792d09dd945f6b26
BlackChaOs <tumitza@gmail.com>
2016-04-03 09:49:22 +0700

    intent webview finish

--- app/src/main/java/aligns/sapphaya/blackchaos/alerttraffic/MainActivity.java
+++ app/src/main/java/aligns/sapphaya/blackchaos/alerttraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package aligns.sapphaya.blackchaos.alerttraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
                 //sound effect
                 MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
                 mediaPlayer.start();
+
+                //set url web wiew
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/SkZXDboDqhk"));
+                startActivity(intent);
+
             } //onclick
         });
     }//main mathod
